dafa5b5dfb5179008031b164b747e6899620c5f1
Alexander Diaz <ajribon@uninorte.edu.co>
2018-02-05 15:58:41 -0500

    Changes way of making operations

--- app/src/main/java/com/example/visitante/s2calculadora/MainActivity.java
+++ app/src/main/java/com/example/visitante/s2calculadora/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.visitante.s2calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +15,36 @@ public class MainActivity extends AppCompatActivity {
     private EditText pseudoinput;
 
     private String result = "";
-    private String operand = "";
+    private String toShow = "";
     private String operation;
     private String storedNumber;
+    int index;
+    
+    private ArrayList<String> numbersNoperations;
+    
+    public int operator(ArrayList<String> operations) {
+        int acum = 0;
+        int flag = 1;  // if flag = 1, add, else subtract
+        for (String op: operations) {
+            switch (op) {
+                case "+":
+                    flag = 1;
+                    break;
+                case "-":
+                    flag = 0;
+                    break;
+                default:
+                    if(flag == 1) {
+                        acum += Integer.valueOf(op);
+                    }
+                    else {
+                        acum -= Integer.valueOf(op);
+                    }
+                    break;
+            }
+        }
+        return acum;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,125 +55,146 @@ public class MainActivity extends AppCompatActivity {
         pseudoinput = findViewById(R.id.pseudoinput);
 
         resultados.setText(result);
-        pseudoinput.setText(operand);
+        pseudoinput.setText(toShow);
+        
+        numbersNoperations = new ArrayList<>();
+        numbersNoperations.add("");
+        index = 0;
     }
 
     public void onClickNine(View view) {
-        operand += '9';
-        pseudoinput.setText(operand);
+        String currentNumber = numbersNoperations.remove(index);
+        currentNumber += '9';
+        numbersNoperations.add(index, currentNumber);
+        toShow += '9';
+        pseudoinput.setText(toShow);
     }
 
     public void onClickEight(View view) {
-        operand += '8';
-        pseudoinput.setText(operand);
+        String currentNumber = numbersNoperations.remove(index);
+        currentNumber += '8';
+        numbersNoperations.add(index, currentNumber);
+        toShow += '8';
+        pseudoinput.setText(toShow);
     }
 
     public void onClickSeven(View view) {
-        operand += '7';
-        pseudoinput.setText(operand);
+        String currentNumber = numbersNoperations.remove(index);
+        currentNumber += '7';
+        numbersNoperations.add(index, currentNumber);
+        toShow += '7';
+        pseudoinput.setText(toShow);
     }
 
     public void onClickSix(View view) {
-        operand += '6';
-        pseudoinput.setText(operand);
+        String currentNumber = numbersNoperations.remove(index);
+        currentNumber += '6';
+        numbersNoperations.add(index, currentNumber);
+        toShow += '6';
+        pseudoinput.setText(toShow);
     }
 
     public void onClickFive(View view) {
-        operand += '5';
-        pseudoinput.setText(operand);
+        String currentNumber = numbersNoperations.remove(index);
+        currentNumber += '5';
+        numbersNoperations.add(index, currentNumber);
+        toShow += '5';
+        pseudoinput.setText(toShow);
     }
 
     public void onClickFour(View view) {
-        operand += '4';
-        pseudoinput.setText(operand);
+        String currentNumber = numbersNoperations.remove(index);
+        currentNumber += '4';
+        numbersNoperations.add(index, currentNumber);
+        toShow += '4';
+        pseudoinput.setText(toShow);
     }
 
     public void onClickThree(View view) {
-        operand += '3';
-        pseudoinput.setText(operand);
+        String currentNumber = numbersNoperations.remove(index);
+        currentNumber += '3';
+        numbersNoperations.add(index, currentNumber);
+        toShow += '3';
+        pseudoinput.setText(toShow);
     }
 
     public void onClickTwo(View view) {
-        operand += '2';
-        pseudoinput.setText(operand);
+        String currentNumber = numbersNoperations.remove(index);
+        currentNumber += '2';
+        numbersNoperations.add(index, currentNumber);
+        toShow += '2';
+        pseudoinput.setText(toShow);
     }
 
     public void onClickOne(View view) {
-        operand += '1';
-        pseudoinput.setText(operand);
+        String currentNumber = numbersNoperations.remove(index);
+        currentNumber += '1';
+        numbersNoperations.add(index, currentNumber);
+        toShow += '1';
+        pseudoinput.setText(toShow);
     }
 
     public void onClickCero(View view) {
-        if(!operand.isEmpty()){
-            operand += '0';
+        if(!toShow.isEmpty()){
+            String currentNumber = numbersNoperations.remove(index);
+            currentNumber += '0';
+            numbersNoperations.add(index, currentNumber);
+            toShow += '0';
         }
         else {
-            operand = "0";
+            toShow = "0";
         }
-        pseudoinput.setText(operand);
+        pseudoinput.setText(toShow);
     }
 
     public void onClickDot(View view) {
-        operand += '.';
-        pseudoinput.setText(operand);
+        toShow += '.';
+        pseudoinput.setText(toShow);
     }
 
     public void onClickEquals(View view) {
-        double a, b, c = 0;
-        a = Double.parseDouble(storedNumber);
-        b = Double.parseDouble(operand);
-        switch (operation) {
-            case "+":
-                c = a + b;
-                break;
-            case "-":
-                c = a - b;
-                break;
-            case "*":
-                c = a * b;
-                break;
-            case "/":
-                c = a / b;
-                break;
+        for (String op: numbersNoperations) {
+            Log.d("TAGASO", op);
         }
-        result = String.valueOf(c);
-        storedNumber = "";
-        operand = "";
-        pseudoinput.setText("");
-        resultados.setText(result);
+        resultados.setText(operator(numbersNoperations) + "");
     }
 
     public void onClickMinus(View view) {
-        storedNumber = operand;
-        operand = "";
-        operation = "-";
-        pseudoinput.setText("");
+        numbersNoperations.add("-");
+        index += 2;
+        numbersNoperations.add("");
+        toShow = toShow + "-";
+        pseudoinput.setText(toShow);
     }
 
     public void onClickPlus(View view) {
-        storedNumber = operand;
-        operand = "";
-        operation = "+";
-        pseudoinput.setText("");
+        numbersNoperations.add("+");
+        index += 2;
+        toShow = toShow + "+";
+        numbersNoperations.add("");
+        pseudoinput.setText(toShow);
     }
 
     public void onClickDiv(View view) {
-        storedNumber = operand;
-        operand = "";
+        storedNumber = toShow;
+        toShow = "";
         operation = "/";
         pseudoinput.setText("");
     }
 
     public void onClickDel(View view) {
-        operand = "";
+        numbersNoperations = new ArrayList<>();
+        numbersNoperations.add("");
+        index = 0;
+        toShow = "";
         storedNumber = "";
         pseudoinput.setText("");
         resultados.setText("");
     }
 
     public void onClickBy(View view) {
-        storedNumber = operand;
-        operand = "";
+        storedNumber = toShow;
+        toShow = "";
         operation = "*";
         pseudoinput.setText("");
     }
